7915535012bbfee7b54b78ef099a1225763e06ac
ogios <2134692955@qq.com>
2024-04-16 10:50:00 +0800

    nothing

---
+++ ui/comp/process.go
@@ -0,0 +1,15 @@
+package comp
+
+// type ANSIShit struct {
+// 	bound *[2]int
+// 	data  []byte
+// 	sub   *ANSIShit
+// }
+//
+// func GetANSIs(s string) {
+// 	for i, v := range s {
+// 	}
+// }
+//
+// func ClipView(s string, block [4]int) string {
+// }
